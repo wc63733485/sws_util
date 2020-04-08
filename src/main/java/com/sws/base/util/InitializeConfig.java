5fd5a6f7ae302da129b82bbe5d212629716d5c9d
wc63733485 <476387578@qq.com>
2020-04-08 09:44:35 +0800

    1-15

--- src/main/java/com/sws/base/util/InitializeConfig.java
+++ src/main/java/com/sws/base/util/InitializeConfig.java
@@ -1,51 +1,40 @@
 package com.sws.base.util;
 
-import com.alibaba.fastjson.JSONObject;
-import com.sws.base.service.BaseService;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Date;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
+//public class InitializeConfig {
+//
+//    static Yaml yaml = new Yaml();
+//
+//
+//    public static JSONObject getConfig() {
+//
+//        URL systemResource = InitializeConfig.class.getResource("bootstrap.yml");
+//        Map load = null;
+//        try {
+//            load = yaml.load(new FileInputStream(systemResource.getFile()));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return new JSONObject(load);
+//    }
+//
+//}
 
 public class InitializeConfig {
 
+    private volatile static InitializeConfig singleton;
 
-    static Yaml yaml = new Yaml();
+    private InitializeConfig (String a,String b,String c){}
 
-    public static JSONObject getConfig() {
-
-        URL systemResource = InitializeConfig.class.getResource("bootstrap.yml");
-        Map load = null;
-        try {
-            load = yaml.load(new FileInputStream(systemResource.getFile()));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    public static InitializeConfig getSingleton(String a,String b,String c) {
+        if (singleton == null) {
+            synchronized (InitializeConfig.class) {
+                if (singleton == null) {
+                    singleton = new InitializeConfig(a,b,c);
+                }
+            }
         }
-//        JSONObject jso = new JSONObject(load);
-
-//        try {
-//            URL url = new URL(jso.getJSONObject("config").getString("url"));
-//            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//            conn.setDoInput(true);
-//            conn.connect();
-//            InputStream is = conn.getInputStream();
-//            load = yaml.load(is);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-
-        return new JSONObject(load);
+        return singleton;
     }
-
 }
 
