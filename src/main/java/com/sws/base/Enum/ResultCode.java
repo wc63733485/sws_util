8ec45ff4da72ecd059d1421d6d2e398ab925b13e
hssws <476387578@qq.com>
2020-09-17 17:53:43 +0800

    9-16

--- src/main/java/com/sws/base/Enum/ResultCode.java
+++ src/main/java/com/sws/base/Enum/ResultCode.java
@@ -3,6 +3,7 @@ package com.sws.base.Enum;
 public enum ResultCode {
     /* 成功 */
     SUCCESS(200, "成功"),
+    USER_LOGIN(201, "用户登录"),
 
     /* 默认失败 */
     COMMON_FAIL(999, "失败"),
@@ -16,13 +17,14 @@ public enum ResultCode {
     /* 用户错误 */
     USER_NOT_LOGIN(2001, "用户未登录"),
     USER_ACCOUNT_EXPIRED(2002, "账号已过期"),
-    USER_CREDENTIALS_ERROR(2003, "密码错误"),
+    USER_CREDENTIALS_ERROR(2003, "账号密码错误"),
     USER_CREDENTIALS_EXPIRED(2004, "密码过期"),
     USER_ACCOUNT_DISABLE(2005, "账号不可用"),
     USER_ACCOUNT_LOCKED(2006, "账号被锁定"),
     USER_ACCOUNT_NOT_EXIST(2007, "账号不存在"),
     USER_ACCOUNT_ALREADY_EXIST(2008, "账号已存在"),
     USER_ACCOUNT_USE_BY_OTHERS(2009, "账号下线"),
+    USER_VERIFY_NOTTIMEOUT(2010, "验证码未过期，禁止频繁获取验证码"),
 
     /* 业务错误 */
     NO_PERMISSION(3001, "没有权限");
